e1bb3fb157f062a7187310d04f944f3e3d1f7c10
domosekai <54519668+domosekai@users.noreply.github.com>
2021-08-13 08:52:43 +0000

    initial config without exposing secret

---
+++ config/config_inc.php
@@ -0,0 +1,18 @@
+<?php
+
+require( 'config_secret.php' );
+
+# --- Anonymous Access / Signup ---
+$g_allow_signup                 = OFF;
+$g_allow_anonymous_login        = ON;
+$g_anonymous_account            = 'guest';
+
+$g_display_errors = array(
+    E_WARNING           => DISPLAY_ERROR_HALT,
+    E_ALL               => DISPLAY_ERROR_INLINE,
+);
+
+$g_show_detailed_errors = ON;
+$g_stop_on_errors = ON;
+
+$g_user_login_valid_regex = '/^[\p{L}\p{N}+_.-]*\p{L}+[\p{L}\p{N}+_.-]*$/u';
