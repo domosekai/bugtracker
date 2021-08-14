e09d6e8fa8ff8775d0ed79b6e1856f158e03bbb6
Yihong Wu <54519668+domosekai@users.noreply.github.com>
2021-08-14 15:34:05 +0000

    update config and translation

--- config/custom_strings_inc.php
+++ config/custom_strings_inc.php
@@ -4,20 +4,24 @@ switch( $g_active_language ) {
 		# custom fields
 		$s_field_carrier = '交通工具';
 		$s_field_province = '省份';
+		$s_field_txtype = '交易类型';
 		break;
 	case 'chinese_traditional':
 		# custom fields
 		$s_field_carrier = '交通工具';
 		$s_field_province = '省份';
+		$s_field_txtype = '交易類別';
 		break;
 	case 'japanese':
 		# custom fields
 		$s_field_carrier = '交通機関';
 		$s_field_province = 'エリア';
+		$s_field_txtype = 'タイプ';
 		break;
 	default:
 		# custom fields
 		$s_field_carrier = 'Carrier';
 		$s_field_province = 'Province';
+		$s_field_txtype = 'Type';
 		break;
 }
