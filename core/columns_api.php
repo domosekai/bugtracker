240e5b41d64579c2e135909a0ad851ba787a7306
Yihong Wu <wu@domosekai.com>
2024-05-18 02:45:04 +0900

    Fix view state column title

--- core/columns_api.php
+++ core/columns_api.php
@@ -730,7 +730,8 @@ function print_column_title_view_state( $p_sort, $p_dir, $p_columns_target = COL
 	echo '<th class="column-view-state">';
 	$t_view_state_text = lang_get( 'view_status' );
 	$t_view_state_icon = ' ' . icon_get( 'fa-lock', '', $t_view_state_text );
-	print_view_bug_sort_link( $t_view_state_icon, 'view_state', $p_sort, $p_dir, $p_columns_target );
+	//print_view_bug_sort_link( $t_view_state_icon, 'view_state', $p_sort, $p_dir, $p_columns_target );
+	echo $t_view_state_icon;
 	print_sort_icon( $p_dir, $p_sort, 'view_state' );
 	echo '</th>';
 }
